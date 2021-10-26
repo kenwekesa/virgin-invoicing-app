34c1e6b36452d8a463b3931ef2a02277a4ab3463
kenwekesa <kenwek1994@gmail.com>
2021-10-26 16:45:10 +0300

    ks

--- invoice/views.py
+++ invoice/views.py
@@ -201,38 +201,27 @@ def viewDocumentInvoice(request, slug):
     except:
         messages.error(request, 'Something went wrong')
         return redirect('invoices')
-
     #fetch all the products - related to this invoice
     products = Product.objects.filter(invoice=invoice)
-
     #Get Client Settings
     p_settings = Settings.objects.get(clientName='Skolo Online Learning')
-
     #Calculate the Invoice Total
     invoiceTotal = 0.0
     if len(products) > 0:
         for x in products:
             y = float(x.quantity) * float(x.price)
             invoiceTotal += y
-
-
-
     context = {}
     context['invoice'] = invoice
     context['products'] = products
     context['p_settings'] = p_settings
     context['invoiceTotal'] = "{:.2f}".format(invoiceTotal)
-
     #The name of your PDF file
     filename = '{}.pdf'.format(invoice.uniqueId)
-
     #HTML FIle to be converted to PDF - inside your Django directory
     #template = get_template('invoice/pdf-template.html')
-
-
     #Render the HTML
     #html = template.render(context)
-
     #Options - Very Important [Don't forget this]
     options = {
           'encoding': 'UTF-8',
@@ -244,21 +233,16 @@ def viewDocumentInvoice(request, slug):
           ],
       }
       #Javascript delay is optional
-
     #Remember that location to wkhtmltopdf
    # config = pdfkit.configuration(wkhtmltopdf='/usr/bin/wkhtmltopdf')
-
     #IF you have CSS to add to template
     #css1 = os.path.join(settings.CSS_LOCATION, 'assets', 'css', 'bootstrap.min.css')
     #css2 = os.path.join(settings.CSS_LOCATION, 'assets', 'css', 'dashboard.css')
-
     #Create the file
     #file_content = pdfkit.from_string(html, False, configuration=config, options=options)
-
     #Create the HTTP Response
     #response = HttpResponse(file_content, content_type='application/pdf')
     #response['Content-Disposition'] = 'inline; filename = {}'.format(filename)
-
     #Return
     #return response
 """
@@ -271,38 +255,27 @@ def viewDocumentInvoice(request, slug):
     except:
         messages.error(request, 'Something went wrong')
         return redirect('invoices')
-
     #fetch all the products - related to this invoice
     products = Product.objects.filter(invoice=invoice)
-
     #Get Client Settings
     p_settings = Settings.objects.get(clientName='Skolo Online Learning')
-
     #Calculate the Invoice Total
     invoiceTotal = 0.0
     if len(products) > 0:
         for x in products:
             y = float(x.quantity) * float(x.price)
             invoiceTotal += y
-
-
-
     context = {}
     context['invoice'] = invoice
     context['products'] = products
     context['p_settings'] = p_settings
     context['invoiceTotal'] = "{:.2f}".format(invoiceTotal)
-
     #The name of your PDF file
     filename = '{}.pdf'.format(invoice.uniqueId)
-
     #HTML FIle to be converted to PDF - inside your Django directory
     #template = get_template('invoice/pdf-template.html')
-
-
     #Render the HTML
     html = template.render(context)
-
     #Options - Very Important [Don't forget this]
     options = {
           'encoding': 'UTF-8',
@@ -314,30 +287,23 @@ def viewDocumentInvoice(request, slug):
           ],
       }
       #Javascript delay is optional
-
     #Remember that location to wkhtmltopdf
     config = pdfkit.configuration(wkhtmltopdf='/usr/bin/wkhtmltopdf')
-
     #Saving the File
     filepath = os.path.join(settings.MEDIA_ROOT, 'client_invoices')
     os.makedirs(filepath, exist_ok=True)
     pdf_save_path = filepath+filename
     #Save the PDF
     pdfkit.from_string(html, pdf_save_path, configuration=config, options=options)
-
-
     #send the emails to client
     to_email = invoice.client.emailAddress
     from_client = p_settings.clientName
     emailInvoiceClient(to_email, from_client, pdf_save_path)
-
     invoice.status = 'EMAIL_SENT'
     invoice.save()
-
     #Email was send, redirect back to view - invoice
     messages.success(request, "Email sent to the client succesfully")
     return redirect('create-build-invoice', slug=slug)
-
 	#view invoices here
 	
 # Create your views here.
