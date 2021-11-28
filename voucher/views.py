1c240637cbb198fcad870d7ab919d60e92a644bf
Kennedy <ken@localhost.localdomain>
2021-11-28 06:31:28 +0300

    kdkd

--- voucher/views.py
+++ voucher/views.py
@@ -18,84 +18,84 @@ from weasyprint import HTML, css
 
 
 def view_voucher(request):
-	
-	#fetch all the products - related to this invoice
-	
+    
+    #fetch all the products - related to this invoice
+    
 
 
-	context = {}
-	'''context['invoice'] = invoice
-	context['products'] = products
-	context['invoiceproduct'] = invoiceproduct'''
+    context = {}
+    '''context['invoice'] = invoice
+    context['products'] = products
+    context['invoiceproduct'] = invoiceproduct'''
 
-	
+    
 
-	return render(request, 'voucher/created_voucher.html', context)
+    return render(request, 'voucher/created_voucher.html', context)
 
 
 def viewPDFInvoice(request, slug):
-	#fetch that invoice
-	try:
-		invoice = Invoice.objects.get(slug=slug)
-		pass
-	except:
-		messages.error(request, 'Something went wrong')
-		return redirect('invoices')
+    #fetch that invoice
+    try:
+        invoice = Invoice.objects.get(slug=slug)
+        pass
+    except:
+        messages.error(request, 'Something went wrong')
+        return redirect('invoices')
 
-	#fetch all the products - related to this invoice
-	#products = Product.objects.filter(invoice=invoice)
+    #fetch all the products - related to this invoice
+    #products = Product.objects.filter(invoice=invoice)
 
-	#Get Client Settings
+    #Get Client Settings
    
 
-	#Calculate the Invoice Total
-	invoiceCurrency = ''
-	invoiceTotal = 0.0
-	"""if len(products) > 0:
-		for x in products:
-			y = float(x.quantity) * float(x.price)
-			invoiceTotal += y
-			invoiceCurrency = x.currency
-			"""
+    #Calculate the Invoice Total
+    invoiceCurrency = ''
+    invoiceTotal = 0.0
+    """if len(products) > 0:
+        for x in products:
+            y = float(x.quantity) * float(x.price)
+            invoiceTotal += y
+            invoiceCurrency = x.currency
+            """
 
 
 
-	"""context = {}
-	context['invoice'] = invoice
-	context['products'] = products
-	context['invoiceTotal'] = "{:.2f}".format(invoiceTotal)
-	context['invoiceCurrency'] = invoiceCurrency"""
+    """context = {}
+    context['invoice'] = invoice
+    context['products'] = products
+    context['invoiceTotal'] = "{:.2f}".format(invoiceTotal)
+    context['invoiceCurrency'] = invoiceCurrency"""
 
-	return render(request, 'invoice/invoice-template.html')
+    return render(request, 'invoice/invoice-template.html')
 
 
 def voucher_template(request):
-	paragraphs = ['first paragraph', 'second paragraph', 'third paragraph']
-	
+    paragraphs = ['first paragraph', 'second paragraph', 'third paragraph']
+    
 
 
-	context = {}
-	if request.method == 'POST':
+    context = {}
+    if request.method == 'POST':
         name = request.POST.get('name')
-        if name == 'voucherform':
+    if name == 'voucherform':
             # Do something here.
-			context['no_of_adults']=request.POST.get('no_of_adults')
-			context['no_of_children'] = request.POST.get('no_of_children')
+            context['no_of_adults']=request.POST.get('no_of_adults')
+            context['no_of_children'] = request.POST.get('no_of_children')
         elif name == 'other_frm':
             # Do something else here.
 
 
-	html_string = render_to_string('voucher/voucher_template.html', context)
-	html = HTML(string=html_string, base_url=request.build_absolute_uri())
-	doc = html.render()
-	pdf =doc.write_pdf()
-	
+    html_string = render_to_string('voucher/voucher_template.html', context)
+    html = HTML(string=html_string, base_url=request.build_absolute_uri())
+    doc = html.render()
+    pdf =doc.write_pdf()
+    
 
-	
+    
    
-	response = HttpResponse(pdf, content_type='application/pdf')
-	#response['Content-Disposition'] = 'attachment; filename="mypdf.pdf"'
-	return response
+    response = HttpResponse(pdf, content_type='application/pdf')
+    #response['Content-Disposition'] = 'attachment; filename="mypdf.pdf"'
+    return response
 
-	
+    
 
