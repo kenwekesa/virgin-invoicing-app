e34f76ea9f0f03d63adad95ccd827f5169437828
Kennedy <ken@localhost.localdomain>
2021-12-27 22:43:22 +0300

    changes

--- voucher/models.py
+++ voucher/models.py
@@ -1,3 +1,93 @@
 from django.db import models
 
+from django.core import validators
+from django.db import models
+from django.template.defaultfilters import slugify
+from django.urls.base import reverse
+from django.utils import timezone
+from uuid import uuid4
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator
+
 # Create your models here.
+
+
+def increment_voucher_number():
+    last_invoice = Voucher.objects.all().order_by('id').last()
+    if not last_invoice:
+        return 'VR00001'
+    invoice_no = last_invoice.number
+    invoice_int = int(invoice_no.split('VR')[-1])
+    new_invoice_int = invoice_int + 1
+    new_invoice_no = 'VR' + "%05d" % (new_invoice_int, )
+    return new_invoice_no
+
+
+class Voucher(models.Model):
+    TERMS = [
+        ('Immediate', 'Immediate'),
+        ('15 days', '15 days'),
+        ('30 days', '30 days'),
+
+    ]
+
+    STATUS = [
+        ('CURRENT', 'CURRENT'),
+        ('EMAIL_SENT', 'EMAIL_SENT'),
+        ('OVERDUE', 'OVERDUE'),
+        ('PAID', 'PAID'),
+    ]
+
+    number = models.CharField(
+        null=False, default=increment_invoice_number, blank=False, max_length=100, unique=True)
+    dueDate = models.DateField(null=True, blank=True)
+    paymentTerms = models.CharField(
+        choices=TERMS, default='15 days', max_length=100)
+    status = models.CharField(
+        choices=STATUS, default='CURRENT', max_length=100)
+    description = models.TextField(null=True, blank=True, max_length=500)
+    istaxable = models.BooleanField()
+
+    # RELATED fields
+    client = models.ForeignKey(
+        Client, blank=True, null=True, on_delete=models.SET_NULL)
+
+    # Utility fields
+    uniqueId = models.CharField(null=True, blank=True, max_length=100)
+    slug = models.SlugField(max_length=500, unique=True, blank=True, null=True)
+    date_created = models.DateTimeField(blank=True, null=True)
+    last_updated = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return '{} {}'.format(self.number, self.uniqueId)
+
+    def get_absolute_url(self):
+        return reverse('invoice-detail', kwargs={'slug': self.slug})
+
+    # def total_price(self):
+    #	return float(100)
+    def total_price(self):
+        temp_values = [int(product.price) * int(product.quantity)
+                       for product in InvoiceProduct.objects.filter(invoice_id=self.id)]
+        return sum(temp_values)
+
+    def grand_total(self):
+        temp_values = 0.00
+        for product in InvoiceProduct.objects.filter(invoice_id=self.id):
+            if product.price and product.quantity:
+                temp_values = temp_values + \
+                    (int(product.price) * int(product.quantity))
+
+        return temp_values
+
+    def save(self, *args, **kwargs):
+        if self.date_created is None:
+            self.date_created = timezone.localtime(timezone.now())
+        if self.uniqueId is None:
+            self.uniqueId = str(uuid4()).split('-')[4]
+            self.slug = slugify('{} {}'.format(self.number, self.uniqueId))
+
+        self.slug = slugify('{} {}'.format(self.number, self.uniqueId))
+        self.last_updated = timezone.localtime(timezone.now())
+
+        super(Voucher, self).save(*args, **kwargs)
