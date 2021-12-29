7a83fb4550e24110690f53376947e964fdb96f1e
Kennedy <ken@localhost.localdomain>
2021-12-29 22:57:14 +0300

    voucher form

--- voucher/forms.py
+++ voucher/forms.py
@@ -5,37 +5,36 @@ from django.forms.widgets import DateInput
 from invoice.models import Invoice
 from crispy_forms.helper import FormHelper
 
+
 class VoucherForm(forms.ModelForm):
     THE_OPTIONS = [
-    ('Immediate', 'Immediate'),
-    ('15 days', '15 days'),
-    ('30 days', '30 days'),
-    ('60 days', '60 days'),
+        ('Immediate', 'Immediate'),
+        ('15 days', '15 days'),
+        ('30 days', '30 days'),
+        ('60 days', '60 days'),
     ]
     STATUS_OPTIONS = [
-    ('CURRENT', 'CURRENT'),
-    ('OVERDUE', 'OVERDUE'),
-    ('PAID', 'PAID'),
+        ('CURRENT', 'CURRENT'),
+        ('OVERDUE', 'OVERDUE'),
+        ('PAID', 'PAID'),
     ]
 
     istaxable = forms.BooleanField(label="Taxable", required=False)
     paymentTerms = forms.ChoiceField(
-                    choices = THE_OPTIONS,
-                    required = True,
-                    label='Payment Terms',
-                    widget=forms.Select(attrs={'class': 'form-control mb-3'}),)
+        choices=THE_OPTIONS,
+        required=True,
+        label='Payment Terms',
+        widget=forms.Select(attrs={'class': 'form-control mb-3'}),)
     status = forms.ChoiceField(
-                    choices = STATUS_OPTIONS,
-                    required = True,
-                    label='Invoice Status',
-                    widget=forms.Select(attrs={'class': 'form-control mb-3'}),)
-
+        choices=STATUS_OPTIONS,
+        required=True,
+        label='Invoice Status',
+        widget=forms.Select(attrs={'class': 'form-control mb-3'}),)
 
     dueDate = forms.DateField(
-                        required = True,
-                        label='Invoice Due',
-                        widget=DateInput(attrs={'class': 'form-control mb-3'}),)
-
+        required=True,
+        label='Invoice Due',
+        widget=DateInput(attrs={'class': 'form-control mb-3'}),)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -54,23 +53,23 @@ class VoucherForm(forms.ModelForm):
 
             Submit('submit', ' EDIT INVOICE '))
 
-        
         self.fields['number'].widget.attrs['readonly'] = True
 
-  
     class Meta:
-        model = Invoice
-        fields = ['number', 'dueDate', 'paymentTerms', 'status', 'description', 'client','istaxable']
+        model = Voucher
+        fields = ['number', 'facility_name', 'number_of_adults',
+                  'number_of_children', 'children_age', 'infants', 'baby_cot', 'accomodation', 'meal_plan', 'extras_to', 'special_instructions',
+                  'reserver_name', 'reservation_date', 'client_name', 'client_email'
+                  ]
 
-    
     def clean(self, *args, **kwargs):
-      super(InvoiceForm, self).clean()
+        super(InvoiceForm, self).clean()
 
-      # getting username and password from cleaned_data
-      number = self.cleaned_data.get('number')
-      dueDate = self.cleaned_data.get('password')
+        # getting username and password from cleaned_data
+        number = self.cleaned_data.get('number')
+        dueDate = self.cleaned_data.get('password')
 
-       # validating the username and password
-      if 'VRG' not in number:
-         self._errors['number'] = self.error_class(['Invalid invoice number'])
-
+        # validating the username and password
+        if 'VRG' not in number:
+            self._errors['number'] = self.error_class(
+                ['Invalid invoice number'])
--- voucher/models.py
+++ voucher/models.py
@@ -12,47 +12,40 @@ from django.core.validators import MinValueValidator
 # Create your models here.
 
 
-
 class Client(models.Model):
 
+    # Basic Fields.
+    clientName = models.CharField(null=True, blank=True, max_length=200)
+    address = models.CharField(null=True, blank=True, max_length=200)
+    postalCode = models.CharField(null=True, blank=True, max_length=10)
+    phoneNumber = models.CharField(null=True, blank=True, max_length=100)
+    emailAddress = models.CharField(null=True, blank=True, max_length=100)
+
+    # Utility fields
+    uniqueId = models.CharField(null=True, blank=True, max_length=100)
+    slug = models.SlugField(max_length=500, unique=True, blank=True, null=True)
+    date_created = models.DateTimeField(blank=True, null=True)
+    last_updated = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return '{} {}'.format(self.clientName, self.uniqueId)
+
+    def get_absolute_url(self):
+        return reverse('client-detail', kwargs={'slug': self.slug})
+
+    def save(self, *args, **kwargs):
+        if self.date_created is None:
+            self.date_created = timezone.localtime(timezone.now())
+        if self.uniqueId is None:
+            self.uniqueId = str(uuid4()).split('-')[4]
+            self.slug = slugify('{} {}'.format(self.clientName, self.uniqueId))
+
+        self.slug = slugify('{} {}'.format(self.clientName, self.uniqueId))
+        self.last_updated = timezone.localtime(timezone.now())
+
+        super(Client, self).save(*args, **kwargs)
 
 
-  
-	#Basic Fields.
-	clientName = models.CharField(null=True, blank=True, max_length=200)
-	address = models.CharField(null=True, blank=True, max_length=200)
-	postalCode = models.CharField(null=True, blank=True, max_length=10)
-	phoneNumber = models.CharField(null=True, blank=True, max_length=100)
-	emailAddress = models.CharField(null=True, blank=True, max_length=100)
-	
-
-
-	#Utility fields
-	uniqueId = models.CharField(null=True, blank=True, max_length=100)
-	slug = models.SlugField(max_length=500, unique=True, blank=True, null=True)
-	date_created = models.DateTimeField(blank=True, null=True)
-	last_updated = models.DateTimeField(blank=True, null=True)
-
-
-	def __str__(self):
-		return '{} {}'.format(self.clientName, self.uniqueId)
-
-
-	def get_absolute_url(self):
-		return reverse('client-detail', kwargs={'slug': self.slug})
-
-
-	def save(self, *args, **kwargs):
-		if self.date_created is None:
-			self.date_created = timezone.localtime(timezone.now())
-		if self.uniqueId is None:
-			self.uniqueId = str(uuid4()).split('-')[4]
-			self.slug = slugify('{} {}'.format(self.clientName, self.uniqueId))
-
-		self.slug = slugify('{} {}'.format(self.clientName, self.uniqueId))
-		self.last_updated = timezone.localtime(timezone.now())
-
-		super(Client, self).save(*args, **kwargs)
 def increment_voucher_number():
     last_invoice = Voucher.objects.all().order_by('id').last()
     if not last_invoice:
@@ -81,17 +74,28 @@ class Voucher(models.Model):
 
     number = models.CharField(
         null=False, default=increment_voucher_number, blank=False, max_length=100, unique=True)
-    dueDate = models.DateField(null=True, blank=True)
-    paymentTerms = models.CharField(
-        choices=TERMS, default='15 days', max_length=100)
-    status = models.CharField(
-        choices=STATUS, default='CURRENT', max_length=100)
-    description = models.TextField(null=True, blank=True, max_length=500)
-    istaxable = models.BooleanField()
+    facility_name = models.CharField(null=True, blank=True)
+    number_of_adults = models.CharField(
+        default='0', max_length=100)
+    number_of_children = models.CharField(
+        default='0', max_length=100)
+    children_age = models.CharField(null=True, blank=True, max_length=500)
+    infants = models.CharField(null=True, blank=True, max_length=500)
+    baby_cot = models.CharField(null=True, blank=True, max_length=500)
+    accomodation = models.CharField(null=True, blank=True, max_length=500)
+    meal_plan = models.CharField(null=True, blank=True, max_length=500)
+    extras_to = models.CharField(null=True, blank=True, max_length=500)
+    special_instructions = models.CharField(
+        null=True, blank=True, max_length=500)
+    reserver_name = models.CharField(null=True, blank=True, max_length=500)
+    reservation_date = models.CharField(null=True, blank=True, max_length=500)
 
+    client_name = models.CharField(null=True, blank=True, max_length=500)
+    client_email = models.CharField(null=True, blank=True, max_length=500)
     # RELATED fields
-    client = models.ForeignKey(
+    """client = models.ForeignKey(
         Client, blank=True, null=True, on_delete=models.SET_NULL)
+    """
 
     # Utility fields
     uniqueId = models.CharField(null=True, blank=True, max_length=100)
